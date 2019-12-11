0e0f08b816a4faa6e6fb8c44d34195f522ae6199
Web <web@it.step>
2019-12-11 19:05:30 +0300

    add file

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ Route::get('/admin-panel','HomeController@dashboard')->name('dashboard');
 Route::get('/admin-panel/posts','PostController@index')->name('posts');
 Route::get('/admin-panel/posts/create','PostController@create');
 Route::post('/admin-panel/posts/store','PostController@store');
+Route::post('/admin-panel/posts/store2','PostController@store2');
 Route::get('/admin-panel/posts/{id}','PostController@show')->name('post.show');
